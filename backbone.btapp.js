aab7a8ca6bda03b9b74479f6a8b5414b07294057
Patrick <patrick@bittorrent.com>
2011-11-18 08:50:52 -0800

    hooked up the functions to call the appropriate urls

--- backbone.btapp.js
+++ backbone.btapp.js
@@ -1,8 +1,24 @@
 $(function() {
 	function assert(b) { if(!b) debugger; }
 
-	function btfunction(url, cb) {
-		debugger;
+	var host = 'http://localhost:22907/';
+
+	function createFunction(url) {
+		return function(cb) {
+			var path = host + url + '(';
+			for(var i = 1; i < arguments.length; i++) {
+				if(i > 1) path += ',';
+				path += arguments[i];
+			}
+			path += ')/';
+			console.log(path);
+			$.ajax({
+				url: path,
+				dataType: 'jsonp', 
+				success: cb,
+				error: cb
+			});		
+		};
 	}
 	
 	//in order for us to put data structures into a backbone collection every child must be an object
@@ -18,7 +34,7 @@ $(function() {
 		initialize: function() {
 			_.bind(this, 'onFetch');
 		},
-		onFetch: function(data) {
+		onFetch: function(data, url) {
 			for(v in data) {
 				var variable = data[v];
 				
@@ -33,16 +49,16 @@ $(function() {
 						model = new FetchModel({id:v});
 						this.add(model);
 					}
-					model.onFetch(variable);
+					model.onFetch(variable, url + v + '/');
 				} else assert(false);
 			}
-		},
+		}
 	});
 	window.FetchModel = Backbone.Model.extend({
 		initialize: function() {
 			_.bind(this, 'onFetch');
 		},
-		onFetch: function(data) {
+		onFetch: function(data, url) {
 			for(var v in data) {
 				var variable = data[v];
 
@@ -56,7 +72,7 @@ $(function() {
 				//special case properties to be a model with a bunch of attributes
 				var param = {};
 				if(v === 'properties') {
-					this.onFetch(variable.all);
+					this.onFetch(variable.all, url + v + '/all/');
 				} else if(typeof variable === 'object' && ('all' in variable && validCollection(variable.all))) {
 					//collection
 					var collection = this.get(v);
@@ -65,12 +81,12 @@ $(function() {
 						param[v] = collection;
 						this.set(param);
 					}
-					collection.onFetch(variable.all);
+					collection.onFetch(variable.all, url + v + '/all/');
 				} else if(variable === '[native function]') {
 					//dangle functions off the object directly
 					if(!(v in this)) {
-						var url = '';
-						this[v] = _.bind(btfunction, url);
+						this[v] = createFunction(url + v);
+						this.trigger('change');
 					}
 				} else {
 					//set as attributes
@@ -78,7 +94,7 @@ $(function() {
 					this.set(param);
 				}	
 			}
-		},
+		}
 	});
 
 	window.Btapp = FetchModel.extend({
@@ -92,20 +108,20 @@ $(function() {
 		},
 		fetch: function() {
 			$.ajax({
-				url: 'http://localhost:22907/btapp/',
+				url: host + 'btapp/',
 				dataType: 'jsonp', 
 				context: this, 
 				success: function(data) {
-					this.onFetch(data.btapp);
+					this.onFetch(data.btapp, 'btapp/');
 				}, 
-				error: this.fetch, //the initial fetch is critical...so keep retrying
+				error: this.fetch //the initial fetch is critical...so keep retrying
 			});
 		},
 		onEvent: function(data) {
 			//there are two types of events...state updates and notifications like add/delete torrent
 			//handle state updates the same way we handle the initial tree building
 			if('btapp' in data) {
-				this.onFetch(data.btapp);
+				this.onFetch(data.btapp, 'btapp/');
 			} else {
 				//check for torrent status events...the only one that isn't captured by update gui
 				//is torrent deletes...so check here
@@ -117,21 +133,21 @@ $(function() {
 		},
 		waitForEvents: function() {
 			$.ajax({
-				url: 'http://localhost:22907/btappevents/',
+				url: host + 'btappevents/',
 				dataType: 'jsonp',
 				context: this,
 				success: function(data) {
 					for(var i = 0; i < data.length; i++) {
 						this.onEvent(data[i]);
 					}
-					setTimeout(this.waitForEvents, 200);
+					setTimeout(this.waitForEvents, 1000);
 				},
 				error: function() {
 					this.onError();
-					setTimeout(this.waitForEvents, 200);
-				},
+					setTimeout(this.waitForEvents, 1000);
+				}
 			});
-		},
+		}
 	});
 	window.btapp = new Btapp();
 });
