5a2a9d909dfc35baa58ada6d3d163840e0652e6c
Patrick <patrick@bittorrent.com>
2011-11-21 10:22:08 -0800

    finishing up restful procedure call support and relaying them into backbone event triggers...handling failures and reconnecting when possible

--- backbone.btapp.js
+++ backbone.btapp.js
@@ -3,14 +3,34 @@ $(function() {
 
 	var host = 'http://localhost:22907/';
 
-	function createFunction(url) {
+	//we can't send function pointers to the torrent client server, so we'll send
+	//the name of the callback, and the server can call this by sending an event with
+	//the name and args back to us...we're responsible for making the call to the function 
+	//when we detect this...this is similar to the way that jsonp makes ajax callbacks
+	window.btappCallbacks = {};
+	function storeCallbackFunction(cb) {
+		var str = 'bt_';
+		for(var i = 0; i < 20; i++) { str += Math.floor(Math.random() * 10); }
+		window.btappCallbacks[str] = cb;
+		return str;
+	}
+	
+	//functions are simply urls that we make ajax request to...the cb is called with the
+	//result of that ajax request.
+	function createFunction(session, url) {
 		return function(cb) {
 			var path = host + url + '(';
 			for(var i = 1; i < arguments.length; i++) {
 				if(i > 1) path += ',';
-				path += arguments[i];
+				//we are responsible for converting functions to variable names...
+				//this will be called later via a event with a callback and arguments variables
+				if(typeof arguments[i] === 'function') {
+					path += storeCallbackFunction(arguments[i]);
+				} else {
+					path += arguments[i];
+				}
 			}
-			path += ')/';
+			path += ')/?session=' + session;
 			console.log(path);
 			$.ajax({
 				url: path,
@@ -21,107 +41,111 @@ $(function() {
 		};
 	}
 	
-	//in order for us to put data structures into a backbone collection every child must be an object
-	function validCollection(collection) {
-		for(c in collection) {
-			if(typeof collection[c] === 'object') continue;
-			else return false;
-		}
-		return true;
-	}
-
-	window.FetchCollection = Backbone.Collection.extend({
-		initialize: function() {
-			_.bind(this, 'onFetch');
-		},
-		onFetch: function(data, url) {
-			for(v in data) {
-				var variable = data[v];
-				
-				if(variable === 'Not yet supported') continue;
-				
-				if(typeof variable === 'object') {
-					assert(!('all' in variable));
-
-					//an object that we need to iterate over
-					var model = this.get(v);
-					if(!model) {
-						model = new FetchModel({id:v});
-						this.add(model);
-					}
-					model.onFetch(variable, url + v + '/');
-				} else assert(false);
-			}
-		}
-	});
 	window.FetchModel = Backbone.Model.extend({
 		initialize: function() {
-			_.bind(this, 'onFetch');
+			_.bindAll(this, 'clearState', 'updateState');
+			this.bt = {};
+			this.url = '';
+			this.session = null;
 		},
-		onFetch: function(data, url) {
+		clearState: function() {
+			for(a in this.attributes) {
+				var attribute = this.attributes[a];
+				if(typeof attribute === 'object' && 'clearState' in attribute) {
+					attribute.clearState();
+				}
+			}
+			this.bt = {};
+			this.clear();
+		},
+		updateState: function(session, data, url) {
+			this.session = session;
+			this.url = url;
 			for(var v in data) {
 				var variable = data[v];
-
-				if(variable === 'Not yet supported') continue;
-
-				//check the type of the variable...
-				//function namespace
-				//function definition
-				//collection
-				
-				//special case properties to be a model with a bunch of attributes
 				var param = {};
-				if(v === 'properties') {
-					this.onFetch(variable.all, url + v + '/all/');
-				} else if(typeof variable === 'object' && ('all' in variable && validCollection(variable.all))) {
-					//collection
-					var collection = this.get(v);
-					if(!collection) {
-						collection = new FetchCollection;
-						param[v] = collection;
-						this.set(param);
+				if(typeof variable === 'object') {
+					//don't recreate a variable we already have...just update it
+					var model = this.get(v);
+					if(!model) {
+						model = new FetchModel;
 					}
-					collection.onFetch(variable.all, url + v + '/all/');
+					model.updateState(this.session, variable, this.url + v + '/');
+					param[v] = model;
+					this.set(param,{server:true});
 				} else if(variable === '[native function]') {
-					//dangle functions off the object directly
-					if(!(v in this)) {
-						this[v] = createFunction(url + v);
+					if(!(v in this.bt)) {
+						this.bt[v] = createFunction(session, this.url + v);
 						this.trigger('change');
 					}
 				} else {
 					//set as attributes
-					param[v] = variable
-					this.set(param);
+					param[v] = variable;
+					this.set(param, {server:true});
 				}	
 			}
+		},
+		set: function(attributes, options) {
+			Backbone.Model.prototype.set.call(this, attributes, options);
+			//TODO: when server isn't set assume that the change we caused by the user
+			//in this case do a set to the server to update the value of the property
+			//if(!options || !('server' in options) || !options['server']) {
 		}
 	});
 
 	window.Btapp = FetchModel.extend({
 		initialize: function() {
-			_.bindAll(this, 'fetch', 'waitForEvents');
-			this.fetch();
-			this.waitForEvents();
+			FetchModel.prototype.initialize.call(this);
+			_.bindAll(this, 'onEvents', 'onFetch', 'onInitializeSession');
+			this.initializeSession();
 		},
-		onError: function() {
-			debugger;
-		},
-		fetch: function() {
+		queryClient: function(query, cb) {
 			$.ajax({
-				url: host + 'btapp/',
+				url: host + query,
 				dataType: 'jsonp', 
 				context: this, 
 				success: function(data) {
-					this.onFetch(data.btapp, 'btapp/');
-				}, 
-				error: this.fetch //the initial fetch is critical...so keep retrying
+					if('error' in data)	this.connectionError();
+					else cb(data);
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					this.connectionError();
+				},
+				timeout: 4000
 			});
 		},
+		connectionError: function() {
+			console.log('connection lost...retrying...');
+			this.clearState();
+			this.initializeSession();
+		},
+		onInitializeSession: function(data) {
+			assert('session' in data);
+			this.session = data.session
+			console.log('connection established...');
+			this.fetch(data.session);
+		},
+		initializeSession: function() {
+			console.log('initializing connection');
+			this.queryClient('btappinit/', this.onInitializeSession);
+		},
+		onFetch: function(data) {
+			assert('btapp' in data);
+			this.updateState(this.session, data.btapp, 'btapp/');
+			this.setEvents();
+			this.waitForEvents();
+		},
+		fetch: function(session) {
+			this.queryClient('btapp/?session=' + this.session, this.onFetch);
+		},
 		onEvent: function(data) {
-			//there are two types of events...state updates and notifications like add/delete torrent
+			//there are two types of events...state updates and callbacks
 			//handle state updates the same way we handle the initial tree building
 			if('btapp' in data) {
-				this.onFetch(data.btapp, 'btapp/');
+				this.updateState(this.session, data.btapp, 'btapp/');
+			} else if('callback' in data && 'arguments' in data) {
+				console.log('CALLBACK - ' + data.callback + '(' + $.toJSON(data.arguments) + ')');
+				window.btappCallbacks[data.callback](data.arguments);
 			} else {
 				//check for torrent status events...the only one that isn't captured by update gui
 				//is torrent deletes...so check here
@@ -131,20 +155,34 @@ $(function() {
 				}
 			}
 		},
+		onEvents: function(data) {
+			for(var i = 0; i < data.length; i++) {
+				this.onEvent(data[i]);
+			}
+			setTimeout(_.bind(this.waitForEvents, this), 250);
+		},
 		waitForEvents: function() {
-			$.ajax({
-				url: host + 'btappevents/',
-				dataType: 'jsonp',
-				context: this,
-				success: function(data) {
-					for(var i = 0; i < data.length; i++) {
-						this.onEvent(data[i]);
-					}
-					setTimeout(this.waitForEvents, 1000);
-				},
-				error: function() {
-					this.onError();
-					setTimeout(this.waitForEvents, 1000);
+			this.queryClient('btappevents/?session=' + this.session, this.onEvents);
+		},
+		setEvents: function() {
+			//we assume that we just filled in the events information...we desperately want to
+			//set these so that get all the callbacks from the client...what we want to do is
+			//just have the default event handler trigger an event that has the same name as the event
+			//so if you're using the model you can just do something like 
+			//btapp.bind('clientMessage', onClientMessage) and your handler will receive the info blog
+			//regarding that type of event...it also multiplexes the event handling very nicely
+			for(var ev in this.get('events').get('all').attributes) {
+				this.get('events').bt['set'](null, ev, _.bind(this.trigger, this, ev));
+			}
+			
+			//we can take advantage of this internally by binding to the torrentStatus event...which we
+			//need to keep an accurate picture of the torrents
+			this.bind('torrentStatus', function(args) {
+				if(args.state == -1 && args.hash) {
+					var torrents = this.get('torrent').get('all');
+					var torrent = torrents.get(args.hash);
+					if(torrent) torrent.clearState();
+					torrents.unset(args.hash);
 				}
 			});
 		}
