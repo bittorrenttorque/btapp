5db5a0f59f9abefa650467a665955ac565cf4697
Patrick <patrick@bittorrent.com>
2011-12-26 20:58:57 -0800

    adding a heatmap app that using a very generic view to show every piece of data, and the amount its been changed

---
+++ examples/heatmap/index_local.js
@@ -0,0 +1,10 @@
+$(function() {
+	window.btappview = new window.BtappView({'model':new Btapp({'id':'btapp'}), 'el':'body'});
+	window.btappview.render();
+	
+	window.btappview.model.bind('add:add', _.bind(function() {
+		var link = 'http://www.clearbits.net/get/1684-captive---bittorrent-edition.torrent';
+		var func = this.get('add').bt['torrent'];
+		func(function() { }, link);
+	}, window.btappview.model));
+});
