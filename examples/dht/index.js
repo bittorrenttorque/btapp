cdcfcaaa921e29c8147aa0c0cce2d313b7b739de
Patrick <patrick@bittorrent.com>
2012-01-05 01:56:55 -0800

    fixing the dht app

--- examples/dht/index.js
+++ examples/dht/index.js
@@ -1,8 +1,7 @@
 $(function() {
-	window.app = new Btapp;
+	app = new Btapp;
 	app.bind('add:dht', function() {
-		btapp.dht.get_any_hash(function(hash) { console.log(
-		window.app.get('dht').bt['get_any_hash'](function() {}, function(hash) {
+		app.get('dht').bt.get_any_hash(function() {}, function(hash) {
 			$('body').append('<div>' + hash + '</div>');
 			window.app.get('add').bt['torrent'](_.bind(function(hash) { console.log('Added ' + hash); }, this, hash), 'magnet:?xt=urn:btih:' + hash + '&tr=http%3A%2F%2Ftracker.publicbt.com%2Fannounce');
 		});
