7dff009d92530521bb9fc22904908ef41673e819
Patrick <patrick@bittorrent.com>
2012-01-19 21:45:52 -0800

    updating the diff example js dependencies

--- examples/diffs/index_local.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-	window.btappview = new window.BtappView({'model':new Btapp({'id':'btapp'}), 'el':'body'});
-	window.btappview.render();
-});
--- examples/diffs/index_remote.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-	window.btappview = new window.BtappView({'model':new Btapp({'username':'username','password':'password'}), 'el':'body'});
-	window.btappview.render();
-});
