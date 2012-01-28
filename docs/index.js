95323e8c193ce446f911497f3678cc921c23d864
Patrick <patrick@bittorrent.com>
2012-01-27 19:49:00 -0800

    pulling most of the model/collection views functionality into a shared base class

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,5 @@
 $(function() {
-	function color() { return '#'+Math.floor(Math.random()*55+200).toString(16)+Math.floor(Math.random()*55+200).toString(16)+Math.floor(Math.random()*55+200).toString(16); }
-
+	var content_visible = null;
 	window.BtappContentView = Backbone.View.extend({
 		tagName: "div",
 		className: "content",
@@ -10,19 +9,22 @@ $(function() {
 			this.model.bind('destroy', this.remove);
 			
 			$(this.el).hide();
-			this.changes = 0;
 		},
 		render: function() {
-			this.changes++;
 			$(this.el).empty();
-			$(this.el).css('background-color', color());
-			
 			if(!this.model.url) return this;
+			this.render_url();
+			this.render_attributes();
+			this.render_functions();
+			return this;
+		},
+		render_url: function() {
 			var html = '';
-			
-			html += '<div class="url"><h4>model changes:<p>' + this.changes + '</p></h4></div>';
 			html += '<div class="url"><h4>url:<p>' + this.model.url + '</p></h4></div>';
-			
+			$(this.el).append(html);
+		},
+		render_attributes: function() {
+			var html = '';
 			//add the attributes
 			html += '<div class="variables"><h4>attributes:';
 			for(var a in this.model.attributes) {
@@ -32,8 +34,10 @@ $(function() {
 				}
 			}
 			html += '</h4></div>';
-			
-			//add function information
+			$(this.el).append(html);
+		},
+		render_functions: function() {
+			var html = '';
 			html += '<div class="functions"><h4>functions:';
 			for(var b in this.model.bt) {
 				var signatures = this.model.bt[b].valueOf().split('(');
@@ -42,44 +46,40 @@ $(function() {
 					html += '<p><span>function</span>(' + signatures[i] + '</p>';
 				}
 			}
-			
 			html += '</h4></div>';
 			$(this.el).append(html);
-			return this;
 		},
 		show: function() {
-			$('.content').hide();
+			if(content_visible) {
+				$(content_visible.el).hide();
+			}
 			$(this.el).show();
+			content_visible = this;
 		}
 	});
 
-	window.BtappCollectionSidebarView = Backbone.View.extend({
+	window.BtappSidebarView = Backbone.View.extend({
 		tagName: "div",
 		initialize: function() {
-			_.bindAll(this, '_add', '_remove', 'render');
-			this.model.bind('add', this._add);
-			this.model.remove('remove', this._remove);
+			Backbone.View.prototype.initialize.apply(this, arguments);	
+			_.bindAll(this, 'render', 'add', 'remove');
+			this.model.bind('add', this.add);
+			this.model.bind('remove', this.remove);
+			this.model.bind('change', this.render);
 			this.model.bind('destroy', this.remove);
-			
-			this.expanded = true;
+			this.expanded = false;
 			this._views = {};
-			this.model.each(this._add);
+
 			this.content = new BtappContentView({'model':this.model});
 			$('#content').append(this.content.render().el);
 		},
-		_add: function(model) {
-			this._views[model.cid] = new BtappModelSidebarView({'model':model});
+		render_label: function() {
+			var toks = this.model.url.split('/');
+			var link = $('<a href="#">' + unescape(toks[toks.length-2]) + '</a>');
+			link.click(this.content.show);
+			$(this.el).append(link);
 		},
-		_remove: function(model) {
-			this._views[model.cid].destructor();
-			delete this._views[model.cid];
-		},
-		render: function() {
-			$(this.el).empty();
-			$(this.el).css('background-color', color());
-
-			if(!this.model.url) return this;
-
+		render_toggle: function() {
 			var toggle = $('<div></div>');
 			toggle.addClass('toggle');
 			if(_.keys(this._views).length > 0) {
@@ -99,12 +99,8 @@ $(function() {
 				}
 				this.expanded = !this.expanded;
 			}, this, toggle));
-
-			var toks = this.model.url.split('/');
-			var link = $('<a href="#">' + unescape(toks[toks.length-2]) + '</a>');
-			link.click(this.content.show);
-			$(this.el).append(link);
-			
+		},
+		render_children: function() {
 			if(_.keys(this._views).length > 0) {
 				var children = $('<div></div>');
 				children.addClass('children');
@@ -116,92 +112,62 @@ $(function() {
 				}
 				$(this.el).append(children);
 			}
+		},
+		render: function() {
+			$(this.el).empty();
+			if(!this.model.url) return this;
+
+			this.render_toggle();
+			this.render_label();
+			this.render_children();
 			return this;
 		}
 	});
-	window.BtappModelSidebarView = Backbone.View.extend({
+	
+	window.BtappCollectionSidebarView = BtappSidebarView.extend({
 		tagName: "div",
 		initialize: function() {
-			_.bindAll(this, 'render', '_add', '_remove');
-			this.model.bind('add', this._add);
-			this.model.bind('remove', this._remove);
-			this.model.bind('change', this.render);
-			this.model.bind('destroy', this.remove);
-			
-			this.expanded = true;
-			this._views = {};
-			_.each(this.model.attributes, _.bind(function(value, key) {
-				this._add(value);
-			}, this));
-			
-			this.content = new BtappContentView({'model':this.model});
-			$('#content').append(this.content.render().el);
+			BtappSidebarView.prototype.initialize.apply(this, arguments);	
+			this.model.each(this.add);
 		},
-		_add: function(attribute) {
+		add: function(model) {
+			this._views[model.cid] = new BtappModelSidebarView({'model':model});
+		},
+		remove: function(model) {
+			this._views[model.cid].destructor();
+			delete this._views[model.cid];
+		}
+	});
+	window.BtappModelSidebarView = BtappSidebarView.extend({
+		tagName: "div",
+		initialize: function() {
+			BtappSidebarView.prototype.initialize.apply(this, arguments);	
+			_.each(this.model.attributes, _.bind(function(value, key) {
+				this.add(value);
+			}, this));
+		},
+		add: function(attribute) {
 			if(typeof attribute === 'object' && 'bt' in attribute) {
 				if('length' in attribute) {
-					this._views[attribute.cid] = new BtappCollectionSidebarView({'model':attribute});
+					this._views[attribute.url] = new BtappCollectionSidebarView({'model':attribute});
 				} else {
-					this._views[attribute.cid] = new BtappModelSidebarView({'model':attribute});
+					this._views[attribute.url] = new BtappModelSidebarView({'model':attribute});
 				}
 			}
 		},
-		_remove: function(attribute) {
+		remove: function(attribute) {
 			if(typeof attribute === 'object' && 'bt' in attribute) {
 				for(var v in this._views) {
-					if(this._views[v].model.cid == attribute.cid) {
+					if(this._views[v].model.url == attribute.url) {
 						this._views[v].model.trigger('destroy');
 					}
 				}
 			}
-		},
-		render: function() {
-			$(this.el).empty();
-			$(this.el).css('background-color', color());
-
-			if(!this.model.url) return this;
-
-			var toggle = $('<div></div>');
-			toggle.addClass('toggle');
-			if(_.keys(this._views).length > 0) {
-				toggle.addClass(this.expanded ? 'down' : 'right');
-			}
-			$(this.el).append(toggle);
-			toggle.click(_.bind(function(toggle) {
-				if(!toggle.hasClass('right') && !toggle.hasClass('down')) return;
-
-				$(this.el).children('.children').toggle();
-				if(toggle.hasClass('right')) {
-					toggle.removeClass('right');
-					toggle.addClass('down');
-				} else {
-					toggle.removeClass('down');
-					toggle.addClass('right');
-				}
-				this.expanded = !this.expanded;
-			}, this, toggle));
-
-			var toks = this.model.url.split('/');
-			var link = $('<a href="#">' + unescape(toks[toks.length-2]) + '</a>');
-			link.click(this.content.show);
-			$(this.el).append(link);
-			
-			if(_.keys(this._views).length > 0) {
-				var children = $('<div></div>');
-				children.addClass('children');
-				for(var v in this._views) {
-					children.append($(this._views[v].render().el));
-				}
-				if(!this.expanded) {
-					children.hide();
-				}
-				$(this.el).append(children);
-			}
-			return this;
 		}
 	});
 
 	window.btappview = new window.BtappModelSidebarView({'model':new Btapp({'id':'btapp', 'url':'btapp/'})});
+	window.btappview.expanded = true;
 	$('#sidebar').append(window.btappview.render().el);
 	btappview.content.show();
 });
