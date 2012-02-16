44e85b670b32dca563f1d4484ccba9a034f4db0d
Patrick <patrick@bittorrent.com>
2012-02-16 13:04:04 -0800

    fixing an error setting inline css

--- plugin.btapp.js
+++ plugin.btapp.js
@@ -50,7 +50,7 @@ jQuery(function() {
 				cb();
 			});
 			var div = document.createElement('div');			
-			div.css({'position':'absolute','left':'-999em'});
+			$(div).css({'position':'absolute','left':'-999em'});
 			div.innerHTML = 
 				'<object id="' + BTAPP_PLUGIN_ID + '" type="' + MIME_TYPE + '" width="0" height="0">' + 
 					'<param name="onload" value="' + onload + '" />' + 
