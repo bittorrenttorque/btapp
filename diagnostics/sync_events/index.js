cb2575ed794d1a258907235ebeb6681f7f5db4da
Patrick <patrick@bittorrent.com>
2012-02-26 21:15:17 -0800

    making the sync events app more verbose

--- diagnostics/sync_events/index.js
+++ diagnostics/sync_events/index.js
@@ -1,15 +1,13 @@
 function print(data, prefix) {
 	prefix = prefix || '';
-	var num_attributes = 0;
 	for(d in data) {
 		if(typeof data[d] === 'object') {
 			console.log(prefix + d);
 			print(data[d], prefix + '    ');
 		} else {
-			num_attributes++;
+			console.log(prefix + d + ': ' + data[d]);
 		}
 	}
-	console.log(prefix + num_attributes + ' non-object attributes');
 }
 
 jQuery(function() {
