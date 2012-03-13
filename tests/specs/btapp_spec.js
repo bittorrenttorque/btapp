afb13cc65dcfe5bf220297da882a7fd7bd468adf
Patrick <patrick@bittorrent.com>
2012-03-13 12:07:27 -0700

    updating the unit tests with a bunch of torrent creation checks

--- tests/specs/btapp_spec.js
+++ tests/specs/btapp_spec.js
@@ -1,5 +1,64 @@
 (function() {
-	describe('Btapp Functional Tests', function() {
+	describe('Functional Tests', function() {
+		describe('Pairing', function() {
+			it('pairs', function() {
+				runs(function() {
+					this.pairing = new Pairing;
+					this.paired = false;
+					this.pairing.bind('pairing:found', _.bind(function(options) {
+						this.paired = true;
+						expect(options.name).toEqual('Torque');
+						options.authorize = false;
+						options.continue = false;
+					}, this));
+					this.pairing.scan();
+				});
+				
+				waitsFor(function() {
+					return this.paired;
+				}, "client pairing", 15000);
+				
+				runs(function() {
+					expect(this.paired).toBeTruthy();
+				});
+			});
+			it('pairs repeatedly to the same port', function() {
+				runs(function() {
+					this.port = undefined;
+					this.pairing = new Pairing;
+					this.paired = false;
+					this.pairing.bind('pairing:found', _.bind(function(options) {
+						this.paired = true;
+						expect(options.name).toEqual('Torque');
+						if(this.port) {
+							expect(options.port).toEqual(this.port);
+						}
+						this.port = options.port;
+						options.authorize = false;
+						options.continue = false;
+					}, this));
+					this.pairing.scan();
+				});
+				
+				waitsFor(function() {
+					return this.paired;
+				}, "client pairing", 15000);
+				
+				runs(function() {
+					expect(this.paired).toBeTruthy();
+					this.paired = false;
+					this.pairing.scan();
+				});
+				
+				waitsFor(function() {
+					return this.paired;
+				}, "client pairing again", 15000);
+				
+				runs(function() {
+					expect(this.paired).toBeTruthy();
+				});
+			});
+		});
 		describe('Btapp Pairing', function() {
 			beforeEach(function() {
 				this.btapp = new Btapp;
