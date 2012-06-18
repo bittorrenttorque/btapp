2825fbe851320779a29b75e0a9c82368f6b0d53c
Patrick Williams <patrick@bittorrent.com>
2012-06-17 17:14:49 -0700

    fixing the version comparison

--- plugin.btapp.js
+++ plugin.btapp.js
@@ -6,6 +6,30 @@
 (function() {
     var LATEST_PLUGIN_VERSION = '4.3.5';
 
+    function versionCompare(minimum, current) {
+        var minimum = '' + minimum; // Convert number to string
+        var current = '' + current;
+        var parseVersion = function(version) {
+            version = /(\d+)\.?(\d+)?\.?(\d+)?/.exec(version);
+            return {
+                major: parseInt(version[1]) || 0,
+                minor: parseInt(version[2]) || 0,
+                patch: parseInt(version[3]) || 0
+            }
+        };
+        minimum = parseVersion(minimum);
+        current = parseVersion(current);
+        if (minimum.major != current.major) {
+            return (current.major > minimum.major);
+        } else if (minimum.minor != current.minor) {
+            return (current.minor > minimum.minor);
+        } else if (minimum.patch != current.patch) {
+            return (current.patch > minimum.patch);
+        } else {
+            return true;
+        }
+    }
+
     function assert(b, err) { if(!b) throw err; }
 
     function isMac() {
@@ -278,6 +302,7 @@
         },
         plugin_up_to_date: function() {
             var version = this.get_plugin().version;
+            var diff = versionCompare(LATEST_PLUGIN_VERSION, version);
             return LATEST_PLUGIN_VERSION === version;
         },
         get_plugin: function() {
