5a701faeecdf35b67d3201f6e0f1f367ec82a5a7
Patrick Williams <pwmckenna@gmail.com>
2012-11-13 15:55:57 -0800

    disable plugin updates

--- plugin.btapp.js
+++ plugin.btapp.js
@@ -190,7 +190,7 @@
         },
 
         plugin_up_to_date_check: function() {
-            if(this.plugin_up_to_date()) {
+            if(true || this.plugin_up_to_date()) {
                 this.plugin_up_to_date_yes();
             } else {
                 this.plugin_up_to_date_no();
